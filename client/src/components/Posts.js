0ca35659b40a59cb7862774cad27247412ce518c
Julio Mojica <juliocmojica@gmail.com>
2017-10-29 20:33:55 -0400

    adding posts for each category

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -3,20 +3,43 @@ import React, { Component } from 'react';
 class Posts extends Component {
   constructor(props){
     super(props);
+    this.state={};
     this.upVote = this.upVote.bind(this);
   }
   componentDidMount(props) {
+    const headers =  { headers: { 'Authorization': 'whatever-you-want' }}
     console.log(this.props, 'props in posts');
     const { name } = this.props.match.params;
-    console.log(name);
+    fetch(`http://localhost:3001/${name}/posts`, headers)
+    .then(response => response.json())
+    .then(posts => {
+      console.log(posts)
+      this.setState({posts});
+    })
+    .catch(error => { console.log(error)})
   }
   upVote() {
     console.log('upvote meth')
+    let score = this.state.posts[0].voteScore;
+    score++
+    this.state.posts[0].setState({voteScore: score});
   }
   render() {
+    console.log(this.state);
+    const { posts } = this.state;
+    console.log(posts);
     return (
       <div>
-        Posts Component
+        {posts ?
+          posts.map(poststuff => (
+            <div  key={poststuff.id}>
+            <h1>{poststuff.author}</h1>
+            <p>{poststuff.voteScore}</p>
+            <button onClick={this.upVote}>Like</button>
+            </div>
+          ))
+          : 'loading'
+        }
       </div>
     )
   }
