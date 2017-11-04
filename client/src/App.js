60eec33973eafdfd33cceea82da5a0d1e2a1a463
Julio Mojica <juliocmojica@gmail.com>
2017-11-03 21:54:50 -0400

    changing client

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Category from './components/Category';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path='/' component={Home} />
-          <Route path='/posts/:name' component={Category} />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-import appReducer from './reducers/index';
 
-const store = createStore(appReducer, {}, applyMiddleware(reduxThunk));
-
-ReactDOM.render(
-  <Provider store={store}>
-  <App />
-  </Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
