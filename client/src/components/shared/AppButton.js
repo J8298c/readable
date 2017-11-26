42de207657f688897b650efe0db4e94cfaefa3bd
Julio Mojica <juliocmojica@gmail.com>
2017-11-25 20:48:02 -0500

    starting from scratch

--- client/src/components/shared/AppButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Button } from 'semantic-ui-react'
-
-const AppButton = (props) => (
-    <Button color={props.color}  content={props.content} onClick={props.onButtonClick} className={props.className}
-    />
-)
-export default AppButton;
