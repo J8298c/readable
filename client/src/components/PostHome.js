821753953704d42803f5757fe47099a7916b3c29
julio <juliocmojica@gmail.com>
2017-11-28 13:21:29 -0500

    adding signle post iew

--- client/src/components/PostHome.js
+++ client/src/components/PostHome.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import { fetchingAPost, likingPost, fetchingComments, addingAComment } from '../actions/index';
-import { Form } from 'semantic-ui-react';
+import { Form, Card, Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import LongPost from './shared/LongPost';
 import Posts from './shared/Posts'
 import ShortPost from './shared/ShortPost';
 import AppButton from './shared/AppButton';
+import Scoring from './shared/Scoring';
+
 import _ from 'lodash';
 import uuid from 'uuid';
+import { convertDate } from './shared/helper';
 
 
 class PostHome extends Component {
@@ -61,7 +64,45 @@ class PostHome extends Component {
                 <div className='post-container'>
                     {
                         post ?
-                            <Posts posts={post} showVote={true} />
+                        <Card key={post.id}>
+                        <Link to={`/posts/${post.id}`}>
+                        <Card.Content>
+                            <Card.Header>
+                                {post.title}
+                            </Card.Header>
+                            <Card.Meta>
+                                {post.author}
+                            </Card.Meta>
+                            <Card.Meta>
+                                {convertDate(post.timestamp)}
+                            </Card.Meta>
+                            <Card.Description>
+                                {post.body}
+                            </Card.Description>
+                            <Card.Meta>
+                                <Grid columns={2}>
+                                    <Grid.Row>
+                                        <Grid.Column>
+                                            Score: {post.voteScore}
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                            Comment: {post.commentCount}
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                            </Card.Meta>
+                        </Card.Content>
+                        </Link>
+                        <Card.Content extra>
+                            <div>
+                                  <Scoring post={post}/> 
+                                <div className='post-actions'>
+                                    <AppButton content='Comment' size='mini' color='violet'/>
+                                    <AppButton content='Delete' size='mini' floated='right' color='red'/>  
+                                </div>
+                            </div>
+                        </Card.Content>
+                    </Card>
                             : 'Loading'
                     }
                     <AppButton content='edit' color='pink' className='edit-post'/>
