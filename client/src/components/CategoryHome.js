6775fd93f3188856d44826fe683fc7b245386f6a
juliomojica <juliocmojica@gmail.com>
2017-12-14 22:19:01 -0500

    category home now showing posts for category

--- client/src/components/CategoryHome.js
+++ client/src/components/CategoryHome.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchingCategoryPosts } from '../actions/index';
+import { fetchingCategoryPosts, votingOnPost } from '../actions/index';
+import { Card, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const center = {
+  width: '75%',
+  margin: '24px auto'
+}
 
 class CategoryHome extends Component {
     componentDidMount(props) {
         const { category } = this.props.match.params
         this.props.fetchingCategoryPosts(category);
     }
+
+    onVote = (id, option, type) => {
+      this.props.votingOnPost(id, option, type);
+    }
+
     render() {
         return (
-            <div>
+            <Card.Group>
                 {
-                    <h1>{this.props.match.params.category}</h1>
+                  this.props.state.catposts ?
+                  this.props.state.catposts.map(post => (
+                    <Card key={post.id} style={center}>
+                    <Card.Content>
+                     <Link to={`/post/${post.id}`}><Card.Header>{post.title}</Card.Header></Link>
+                      <Card.Meta>{post.author}</Card.Meta>
+                      <Card.Description>{post.body}</Card.Description>
+                      <Card.Meta>
+                        <p>Comments: {post.commentCount}</p>
+                        <p>Score: {post.voteScore}</p>
+                      </Card.Meta>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <Button.Group style={center}>
+                        <Button color='green' content='Like'
+                          onClick={() => {this.onVote(post.id, 'upVote',
+                        'catposts')}} />
+                          <Button.Or />
+                          <Button color='red' content='UnLike'
+                          onClick={() => {this.onVote(post.id, 'downVote', 'catposts')}} />
+                      </Button.Group>
+                    </Card.Content>
+                  </Card>
+                  ))
+                  : null
                 }
-            </div>
+            </Card.Group>
         )
     }
 }
 function mapStateToProps(state) {
+  console.log(state);
     return {
         state
     }
 }
-export default connect(mapStateToProps, { fetchingCategoryPosts })(CategoryHome);
+export default connect(mapStateToProps, { fetchingCategoryPosts, votingOnPost })(CategoryHome);
